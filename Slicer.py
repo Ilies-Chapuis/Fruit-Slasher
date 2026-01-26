9c03a5131d6f4c8cd323010606f54f59e3fd3f85
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-01-26 13:31:41 +0100

    Added Defs, Added Comments

---
+++ Slicer.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+
+def menu_display():
+# Ajoutez des bruitages, musique d'ambiance, niveau de difficulté (fréquence d'apparition, nombres de fruits/bombes/bonus max sur l'écran)
+# Option jeu
+
+def settings():
+
+
+def languages():
+# Support multilingue dans les paramètres
+
+def strikes():
+# Chaque fruit raté/non tranché, donne un strike (à un maximum de 3 Strikes)
+
+def errors():
+# entrées illégales
+# messages de victoire/défaite
+# gestion de sortie du jeu
