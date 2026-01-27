b63e02236fd8bfc0716028d127c4dee976936db4
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-01-27 12:04:20 +0100

    Added Stat Chances

--- config.py
+++ config.py
@@ -2,16 +2,58 @@ WIDTH, HEIGHT = 800, 600
 FPS = 60
 
 FRUITS = [
-    "apple", "banana", "cherry", "kiwi",
-    "orange", "mango", "pear", "melon"
+    "Avocado", "Banana", "Cherry", "Grapes", "Green Apple", "Kiwi", "Lemon", "Melon", "Orange", "Peach", "Pear", "Pinneaple", "Red Apple", "Strawberry", "Watermelon"
 ]
 
+DIFFICULTY_CONFIG =  "Easy", "Normal", "Hard"
+
+if DIFFICULTY_CONFIG == "Easy":
+    FRUITS_CHANCE = 0.85
+    FRUITS_SPEED_MIN =
+    FRUITS_SPEED_MAX =
+    BOMB_CHANCE = 0.05
+    BOMB_SPEED_MIN =
+    BOMB_SPEED_MAX =
+    ICE_CHANCE = 0.10
+    ICE_SPEED_MIN =
+    ICE_SPEED_MAX =
+    GENERAL_SPAWN_RATE =
+    MAX_WINDOW = 7
+
+elif DIFFICULTY_CONFIG == "Normal":
+    FRUITS_CHANCE = 0.65
+    FRUITS_SPEED_MIN =
+    FRUITS_SPEED_MAX =
+    BOMB_CHANCE = 0.15
+    BOMB_SPEED_MIN =
+    BOMB_SPEED_MAX =
+    ICE_CHANCE = 0.15
+    ICE_SPEED_MIN =
+    ICE_SPEED_MAX =
+    GOLDEN_CHANCE = 0.5
+    GOLDEN_SPEED_MIN =
+    GOLDEN_SPEED_MAX =
+    GENERAL_SPAWN_RATE =
+    MAX_WINDOW = 10
+
+elif DIFFICULTY_CONFIG == "Hard":
+    FRUITS_CHANCE = 0.50
+    FRUITS_SPEED_MIN =
+    FRUITS_SPEED_MAX =
+    BOMB_CHANCE = 0.30
+    BOMB_SPEED_MIN =
+    BOMB_SPEED_MAX =
+    ICE_CHANCE = 0.5
+    ICE_SPEED_MIN =
+    ICE_SPEED_MAX =
+    GOLDEN_CHANCE = 0.15
+    GOLDEN_SPEED_MIN =
+    GOLDEN_SPEED_MAX =
+    GENERAL_SPAWN_RATE =
+
+
 BOMB_WORD = "bomb"
-BOMB_CHANCE = 0.15  # 15%
 
-FRUIT_SPEED_MIN = 1
-FRUIT_SPEED_MAX = 3
-
-SPAWN_DELAY = 1300
+SPAWN_RATE = 1300
 MAX_LIVES = 3
 FONT_SIZE = 32
